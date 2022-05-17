c1bbebf1b6147380d79c8d7fda77d91d5236df08
elizabethvera-atieno <atienolizvera@gmail.com>
2022-05-17 09:41:23 +0300

    complete test

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import React, {useEffect, useState} from "react";
+
+function App(){
+    const [images, setImages] = useState(null)
+    // const [isLoaded, setIsLoaded] = useState([true])
+
+    useEffect(()=>{
+       fetch("https://dog.ceo/api/breeds/image/random")
+            .then((r)=> r.json())
+            .then((data)=>{
+                // setIsLoaded(true);
+                setImages(data.message);
+                console.log(data)
+            })
+    },[])
+
+    if(!images) return <p>Loading...</p>;
+
+    return(
+        <div>
+            <h1>Lovely Dog</h1>
+            <img src={images} alt="A Random Dog"/>
+        </div>
+    );
+}
+
+export default App;
